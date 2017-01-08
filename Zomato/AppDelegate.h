a3c8c2be1fd100c5ab872e64edfffa41ae392ba8
Sanni Prasad <sanniprasad@sannis-mac.local>
2017-01-09 00:55:04 +0530

    Initial Commit

---
+++ Zomato/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Zomato
+//
+//  Created by Sanni Prasad on 09/01/17.
+//  Copyright © 2017 Sanni Prasad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Zomato/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Zomato
+//
+//  Created by Sanni Prasad on 09/01/17.
+//  Copyright © 2017 Sanni Prasad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
